9322875b861812ff942d4036e024600f34e4cff3
jnt0r <jona.pollert@gmail.com>
2019-10-15 21:24:13 +0200

    extract Note enum to own file

--- src/test/java/ChangeCalculatorTest.java
+++ src/test/java/ChangeCalculatorTest.java
@@ -7,28 +7,6 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-enum Note {
-    ONECENT(0.01),
-    TWOCENT(0.02),
-    FIVECENT(0.05),
-    TENCENT(0.1),
-    TWENTYCENT(0.2),
-    FIFTYCENT(0.5),
-    ONEEURO(1.),
-    TWOEURO(2.0),
-    FIVEEURO(5.0),
-    TENEURO(10.0);
-
-    private long value;
-
-    private Note(double value) {
-        this.value = (long) (value*100);
-    }
-
-    public long getValue() {
-        return value;
-    }
-}
 public class ChangeCalculatorTest {
 
     private Map<Note, Long> available = new HashMap<>();
